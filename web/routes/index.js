f0f91739161914f7b178e35ac68d6c30e9f57f45
Alvaro Bonilla LLedo <alvaro.bonilla.lledo@gmail.com>
2024-11-08 11:46:06 +0100

    finish

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
 
-/* GET home page. */
+var datos   = require("../data/dataprovider");
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const articulos   = datos.getAllPosts();
+  const categorias  = datos.getCategories();
+  res.render('home', { articles: articulos, categories: categorias });
+});
+
+router.get('/entrada', function(req,res){
+  const articulos   = datos.getAllPosts();
+  const categorias  = datos.getCategories();
+  res.render('home', { articles: articulos, categories: categorias });
+});
+
+router.get('/entradas/:id', function(req,res){
+  const entrada = datos.getPostById(req.params.id);
+  res.send(entrada)
 });
 
 module.exports = router;
